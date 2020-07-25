b37b8729e4ae734824763a56c7d8ffe477eda655
Georgiy Tolparov <tolparge@yandex.ru>
2020-07-25 23:56:23 +0300

    connect to mongodb

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
@@ -15,6 +17,15 @@ app.use(
   })
 );
 
+mongoose.connect(
+  'mongodb+srv://georgyy:123georgyy123@cluster0.o4cea.mongodb.net/carsproject?retryWrites=true&w=majority',
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+
+mongoose.connection.once('open', () => {
+  console.log('has been connected to database');
+});
+
 app.listen(PORT, () => {
-  console.log('server has started on PORT: ' + PORT);
+  console.log('server has been started on PORT: ' + PORT);
 });
