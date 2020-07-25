6a192854b62936c1ce1e658040c13faa4c484dec
Georgiy Tolparov <tolparge@yandex.ru>
2020-07-25 23:33:27 +0300

    add schema

--- server/models/car.js
+++ server/models/car.js
@@ -2,4 +2,8 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const car = new Schema({});
+const car = new Schema({
+  model: String,
+  color: String,
+  personId: String,
+});
