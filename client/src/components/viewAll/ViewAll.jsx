b2d8d02b536eee168cdc9ae49c0c77a3903553bf
Georgiy Tolparov <tolparge@yandex.ru>
2020-07-27 15:23:47 +0300

    add display all persons in database

--- client/src/components/viewAll/ViewAll.jsx
+++ client/src/components/viewAll/ViewAll.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
-class ViewAll extends Component {
-  render() {
-    return <div>Hello ViewAll</div>;
+const getPersonQuery = gql`
+  {
+    persons {
+      name
+      age
+      gender
+      id
+    }
   }
-}
+`;
 
-export default ViewAll;
+const ViewAll = ({ data }) => {
+  function displayPersons() {
+    if (data.loading) {
+      return <div>Loading persons...</div>;
+    } else {
+      return data.persons.map((person, index) => {
+        return <li key={index}>{person.name}</li>;
+      });
+    }
+  }
+  return (
+    <div>
+      <h1>Hello</h1>
+      {displayPersons()}
+    </div>
+  );
+};
+
+export default graphql(getPersonQuery)(ViewAll);
