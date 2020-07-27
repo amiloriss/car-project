a88367a332aa844f51ed3dce3d5b8b882f8c4823
Georgiy Tolparov <tolparge@yandex.ru>
2020-07-27 15:22:31 +0300

    btn to open side navigation

--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
+import './Menu.scss';
 import { Link } from 'react-router-dom';
 
-const Menu = () => {
+const Menu = ({ isOpen, isOpenHandler }) => {
   return (
-    <nav>
-      <ul>
-        <Link to="/addperson">
-          <li>create person</li>
-        </Link>
-        <Link to="/addcar">
-          <li>create car</li>
-        </Link>
-        <Link to="/viewall">
-          <li>view all person</li>
-        </Link>
-        <Link to="/info">
-          <li>information</li>
-        </Link>
-      </ul>
-    </nav>
+    <div className="nav-wrapper">
+      <div
+        className="menu-circle"
+        onClick={isOpenHandler}
+        style={{
+          backgroundColor: isOpen ? '#e9c46a' : '#264653',
+          transform: isOpen ? 'translateX(100%)' : 'translateX(0%)',
+          transition: '0.3s transform',
+        }}
+      ></div>
+      {isOpen ? (
+        <nav className="navigate">
+          <ul>
+            <Link to="/addperson">
+              <li onClick={isOpenHandler}>create person</li>
+            </Link>
+            <Link to="/addcar">
+              <li onClick={isOpenHandler}>create car</li>
+            </Link>
+            <Link to="/viewall">
+              <li onClick={isOpenHandler}>view all person</li>
+            </Link>
+            <Link to="/info">
+              <li onClick={isOpenHandler}>information</li>
+            </Link>
+          </ul>
+        </nav>
+      ) : null}
+    </div>
   );
 };
 
