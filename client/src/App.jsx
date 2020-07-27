596958e0dc995e51e49aacf5c88233fad0bc92d8
Georgiy Tolparov <tolparge@yandex.ru>
2020-07-27 15:24:52 +0300

    connect client to backend with apollocd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
+import './App.scss';
 import Menu from './components/menu/Menu';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import { Route } from 'react-router-dom';
 import Information from './components/information/Information';
 import AddCar from './components/addCar/AddCar';
 import AddPerson from './components/addPerson/AddPerson';
 import ViewAll from './components/viewAll/ViewAll';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+});
+
 class App extends Component {
+  state = {
+    isOpen: false,
+  };
+  isOpenHandler = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
   render() {
     return (
-      <div>
-        <Menu />
-        <div className="app-wrapper">
-          <Route path="/addperson" component={AddPerson} />
-          <Route path="/addcar" component={AddCar} />
-          <Route path="/viewall" component={ViewAll} />
-          <Route path="/info" component={Information} />
+      <ApolloProvider client={client}>
+        <div className="container">
+          <div className="menu-wrapper">
+            <Menu
+              isOpen={this.state.isOpen}
+              isOpenHandler={this.isOpenHandler}
+            />
+          </div>
+          <div className="app-wrapper">
+            <Route path="/addperson" component={AddPerson} />
+            <Route path="/addcar" component={AddCar} />
+            <Route path="/viewall" component={ViewAll} />
+            <Route path="/info" component={Information} />
+          </div>
         </div>
-      </div>
+      </ApolloProvider>
     );
   }
 }
