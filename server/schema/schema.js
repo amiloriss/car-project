49b6f9571e9d8fff96b5107e92cb580acfad5e00
Georgiy Tolparov <tolparge@yandex.ru>
2020-07-27 11:09:49 +0300

    add some query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,26 +4,40 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
+  GraphQLID,
 } = require('graphql');
-const _ = require('lodash');
 
-const persons = [{ name: 'Dasha', age: 20, gender: 'male' }];
-const cars = [{ model: 'BMW', color: 'black' }];
+const Person = require('./../models/person');
+const Car = require('./../models/car');
 
 const PersonType = new GraphQLObjectType({
   name: 'Person',
   fields: () => ({
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
     gender: { type: GraphQLString },
+    cars: {
+      type: new GraphQLList(CarType),
+      resolve(parent, args) {
+        return Car.find({ carId: parent.id });
+      },
+    },
   }),
 });
 
 const CarType = new GraphQLObjectType({
-  name: 'Cars',
+  name: 'Car',
   fields: () => ({
+    id: { type: GraphQLID },
     model: { type: GraphQLString },
     color: { type: GraphQLString },
+    person: {
+      type: PersonType,
+      resolve(parent, args) {
+        return Person.findById(parent.personId);
+      },
+    },
   }),
 });
 
@@ -31,27 +45,74 @@ const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
   fields: {
     persons: {
-      type: PersonType,
+      type: GraphQLList(PersonType),
       resolve(parent, args) {
-        return _.find(persons);
+        return Person.find({});
       },
     },
     cars: {
-      type: CarType,
+      type: GraphQLList(CarType),
       resolve(parent, args) {
-        return _.find(cars);
+        return Car.find({});
       },
     },
     person: {
       type: PersonType,
       args: {
-        name: { type: GraphQLString },
+        id: { type: GraphQLID },
       },
       resolve(parent, args) {
-        return _.find();
+        return Car.findById(args.id);
+      },
+    },
+    car: {
+      type: CarType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return Car.findById(args.id);
       },
     },
   },
 });
 
-module.exports = new GraphQLSchema({ query: RootQuery });
+const Mutation = new GraphQLObjectType({
+  name: 'Mutatuion',
+  fields: () => ({
+    addPerson: {
+      type: PersonType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        gender: { type: GraphQLString },
+        carId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        let person = new Person({
+          name: args.name,
+          age: args.age,
+          gender: args.gender,
+          carId: args.carId,
+        });
+        return person.save();
+      },
+    },
+    addCar: {
+      type: CarType,
+      args: {
+        model: { type: GraphQLString },
+        color: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        let car = new Car({
+          model: args.model,
+          color: args.color,
+        });
+        return car.save();
+      },
+    },
+  }),
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
